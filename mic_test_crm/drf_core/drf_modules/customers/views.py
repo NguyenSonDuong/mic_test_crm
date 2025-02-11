8dfb774b837961c98c4f802cf52d31276c9f5c67
nguyensonduong <nguyenduong08041999@gmail.com>
2025-02-11 11:54:51 +0700

    thêm permission cho project

--- mic_test_crm/drf_core/drf_modules/customers/views.py
+++ mic_test_crm/drf_core/drf_modules/customers/views.py
@@ -19,32 +19,32 @@ class CustomersSearchAPIView(APIView):
             return Response(serializer.data)
         return Response({"error": "Vui lòng nhập từ khóa tìm kiếm!"}, status=400)
     
-class CustomersRegisterAPIView(APIView):
-    queryset = Customers.objects.all()
-    serializer_class = CustomersSerializer
-    def post(self, request):
-        serializer = CustomersSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+# class CustomersRegisterAPIView(APIView):
+#     queryset = Customers.objects.all()
+#     serializer_class = CustomersSerializer
+#     def post(self, request):
+#         serializer = CustomersSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
-class CustomersLoginAPIView(APIView):
-    # authentication_classes = [JWTAuthentication]
-    # permission_classes = [IsAuthenticated]
+# class CustomersLoginAPIView(APIView):
+#     # authentication_classes = [JWTAuthentication]
+#     # permission_classes = [IsAuthenticated]
 
-    def get(self, request):
-        username = request.data.get("username")
-        password = request.data.get("password")
+#     def get(self, request):
+#         username = request.data.get("username")
+#         password = request.data.get("password")
 
-        user = authenticate(username=username, password=password)
+#         user = authenticate(username=username, password=password)
         
-        if user is not None:
-            refresh = RefreshToken.for_user(user)
-            return Response({
-                "refresh": str(refresh),
-                "access": str(refresh.access_token),
-                "role": user.role  # Nếu User model có trường role
-            })
+#         if user is not None:
+#             refresh = RefreshToken.for_user(user)
+#             return Response({
+#                 "refresh": str(refresh),
+#                 "access": str(refresh.access_token),
+#                 "role": user.role  # Nếu User model có trường role
+#             })
         
-        return Response({"error": "Thông tin đăng nhập không hợp lệ"}, status=status.HTTP_401_UNAUTHORIZED)
+#         return Response({"error": "Thông tin đăng nhập không hợp lệ"}, status=status.HTTP_401_UNAUTHORIZED)
--- mic_test_crm/drf_core/drf_modules/user/urls.py
+++ mic_test_crm/drf_core/drf_modules/user/urls.py
@@ -6,7 +6,7 @@ from .views import CustomersRegisterAPIView, CustomersSearchAPIView,CustomersLog
 
 urlpatterns = [
     path('search', CustomersSearchAPIView.as_view(), name='customers-search'),
-    # path('register', CustomersRegisterAPIView.as_view(), name='customers-register'),
-    # path('login', CustomersLoginAPIView.as_view(), name='customers_login'),
-    # path('token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
+    path('register', CustomersRegisterAPIView.as_view(), name='customers-register'),
+    path('login', CustomersLoginAPIView.as_view(), name='customers_login'),
+    path('token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
 ]
